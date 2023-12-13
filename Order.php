a707f5b230ee72296f74be09d758955bdc267197
Rostyslav Bilanenko <dexter.sinatraa@gmail.com>
2023-12-14 00:51:11 +0200

    first commit

---
+++ Order.php
@@ -0,0 +1,40 @@
+<?php
+
+class Order
+{
+    public function calculateTotalSum(){}
+    public function getItems(){}
+    public function getItemCount(){}
+    public function addItem(){}
+    public function deleteItem(){}
+
+    public function printOrder(){}
+    public function showOrder(){}
+
+    public function load(){}
+    public function save(){}
+    public function update(){}
+    public function delete(){}
+}
+class Order1
+{
+    public function calculateTotalSum(){}
+    public function getItems(){}
+    public function getItemCount(){}
+    public function addItem(){}
+    public function deleteItem(){}
+}
+
+class OrderRepository
+{
+    public function load(){}
+    public function save(){}
+    public function update(){}
+    public function delete(){}
+}
+
+class OrderViewer
+{
+    public function printOrder(){}
+    public function showOrder(){}
+}
